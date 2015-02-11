cfa3d33d4aa1aa02c1a93d155333534b3019cc24
jordanroosma <jroosma@ramapo.edu>
2015-02-10 22:09:53 -0500

    Deleted original activity and xml file and replaced each with second screen activity / xml that I haven't modified at all. The code contained in them is only the preloaded code that was generated when I created the new activity.

--- app/src/main/java/org/problets/helloworld/MainActivity.java
+++ app/src/main/java/org/problets/helloworld/MainActivity.java
@@ -1,51 +1,24 @@
 package org.problets.helloworld;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.content.Intent;
 
 
 public class MainActivity extends Activity {
-	public static final String EXTRA_MESSAGE = "org.problets.helloworld.MESSAGE";
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    public static final String EXTRA_MESSAGE = "org.problets.helloworld.MESSAGE";
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
-	/* destination is the location of the tile to be moved,
-	 * source is the location of the blank space
-	 * Returns the direction in which the tile was moved */
-	private String getDirection( int destination, int source )
-	{
-		switch( destination - source )
-		{
-		case -1: return "right";
-		case 1: return "left";
-		case -3: return "down";
-		case 3: return "up";
-		default: return "";
-		}
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
 
-
-
-	/** Converts number to string, 0 to blank string */
-	private String convert( int number )
-	{
-		if( 0 == number )
-			return " ";
-		else
-			return Integer.toString( number );
-	}
 }
