c610ab613576c84af28e3ca3c59bc3c9b46dbb60
Alex Kuhles <alexak75@gmail.com>
2015-02-10 19:06:11 -0500

    Added getters and setters to Point

--- app/src/main/java/org/problets/helloworld/Point.java
+++ app/src/main/java/org/problets/helloworld/Point.java
@@ -4,5 +4,22 @@ package org.problets.helloworld;
  * Created by Alex on 2/10/2015.
  */
 public class Point {
+    private int x_value, y_value;
+
+    public int getX() {
+        return this.x_value;
+    }
+
+    public int getY() {
+        return this.y_value;
+    }
+
+    public void setX(int value) {
+        this.x_value = value;
+    }
+
+    public void setY(int value) {
+        this.y_value = value;
+    }
 
 }
