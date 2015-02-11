42b648fbfc9c66d16f75f862b4807daeda03ac11
jordanroosma <jroosma@ramapo.edu>
2015-02-11 10:31:28 -0500

    Modified EditText field to accept number as inputType and send that number to SecondScreenActivity. Must figure out way to print number on second screen activity to make sure it is getting the correct input...

--- app/src/main/java/org/problets/helloworld/MainActivity.java
+++ app/src/main/java/org/problets/helloworld/MainActivity.java
@@ -1,8 +1,11 @@
 package org.problets.helloworld;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -21,4 +24,27 @@ public class MainActivity extends Activity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onSendUserDimension(View view) {
+
+        Intent getDimensionScreenIntent = new Intent (this, SecondScreenActivity.class);
+
+        getDimensionScreenIntent.putExtra("callingActivity", "MainActivity");
+
+        startActivity(getDimensionScreenIntent);
+    }
 }
--- app/src/main/java/org/problets/helloworld/SecondScreenActivity.java
+++ app/src/main/java/org/problets/helloworld/SecondScreenActivity.java
@@ -1,6 +1,7 @@
 package org.problets.helloworld;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,7 +12,11 @@ public class SecondScreenActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_second_screen);
+
+        Intent activityThatCalled = getIntent();
+
     }
 
 
