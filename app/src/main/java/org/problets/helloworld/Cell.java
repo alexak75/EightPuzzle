0bbc07d59bcfb41cfd3ced21dddce3e1c44432d8
Alex <alexak75@gmail.com>
2015-02-12 16:49:34 -0500

    Added draw functions and getters

--- app/src/main/java/org/problets/helloworld/Cell.java
+++ app/src/main/java/org/problets/helloworld/Cell.java
@@ -1,9 +1,13 @@
 package org.problets.helloworld;
 
+import android.graphics.Canvas;
+
 /**
  * Created by Alex on 2/10/2015.
  */
 public class Cell {
+    private Canvas canvas;
+    private Painter painter;
     private Wall top, left, bottom, right;
 
     public Cell(Wall t, Wall l, Wall b, Wall r) {
@@ -14,19 +18,29 @@ public class Cell {
     }
 
     void drawTopWall() {
-
+        painter.drawWall(canvas, this.top);
     }
-
     void drawBottomWall() {
-
+        painter.drawWall(canvas, this.bottom);
     }
-
     void drawLeftWall() {
-
+        painter.drawWall(canvas, this.left);
+    }
+    void drawRightWall() {
+        painter.drawWall(canvas, this.right);
     }
 
-    void drawRightWall() {
-
+    Wall getTopWall() {
+        return this.top;
+    }
+    Wall getBottomWall() {
+        return this.bottom;
+    }
+    Wall getLeftWall() {
+        return this.left;
+    }
+    Wall getRightWall() {
+        return this.right;
     }
 
 }
