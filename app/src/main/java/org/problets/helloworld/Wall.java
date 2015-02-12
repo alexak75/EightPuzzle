abea012334dc18e5b59c0b427a1a5dcd7356ee81
Alex <alexak75@gmail.com>
2015-02-12 16:54:59 -0500

    Added equals function to Wall

--- app/src/main/java/org/problets/helloworld/Wall.java
+++ app/src/main/java/org/problets/helloworld/Wall.java
@@ -10,12 +10,17 @@ public class Wall {
     private Point p1, p2;
 
     public Wall()  {
-        // Default wall
+    // Default wall
         p1.setCoordinates(0, 0);
         p2.setCoordinates(0, 0);
     }
     public Wall(Point x, Point y) {
         p1 = x;
         p2 = y;
+    }
+
+    boolean equals(Wall w) {
+    // Returns true if the walls have the same two points
+        return (this.p1 == w.p1) && (this.p2 == w.p2);
     }
 }
